8d8bce12066b82f213ede2f9dde417ad2ba684f3
Hugh <hugh_gleaves@hotmail.com>
2022-10-25 12:54:36 -0700

    This code sends and gets legit responses to sent commands.

--- NucleoF446RE/EmbeddedProject4/EmbeddedProject4.cpp
+++ NucleoF446RE/EmbeddedProject4/EmbeddedProject4.cpp
@@ -18,21 +18,25 @@ void SysTick_Handler(void)
 #define SPI_CS GPIO_PIN_4
 #define NRF_CE GPIO_PIN_1
 
+void spi_cs_lo();
+void spi_cs_hi();
+
 void GenerateTestSPISignal()
 {
 	
     __SPI1_CLK_ENABLE();
 	static SPI_HandleTypeDef spi = { .Instance = SPI1 };
-	spi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
-	spi.Init.Direction = SPI_DIRECTION_2LINES;
-	spi.Init.CLKPhase = SPI_PHASE_2EDGE;
-	spi.Init.CLKPolarity = SPI_POLARITY_HIGH;
-	spi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLED;
-	spi.Init.DataSize = SPI_DATASIZE_8BIT;
-	spi.Init.FirstBit = SPI_FIRSTBIT_LSB;
-	spi.Init.NSS = SPI_NSS_SOFT;
-	spi.Init.TIMode = SPI_TIMODE_DISABLED;
 	spi.Init.Mode = SPI_MODE_MASTER; 
+	spi.Init.Direction = SPI_DIRECTION_2LINES;
+	spi.Init.DataSize = SPI_DATASIZE_8BIT;
+	spi.Init.CLKPolarity = SPI_POLARITY_LOW;
+	spi.Init.CLKPhase = SPI_PHASE_1EDGE;
+	spi.Init.NSS = SPI_NSS_SOFT;
+	spi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+	spi.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	spi.Init.TIMode = SPI_TIMODE_DISABLED;
+	spi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLED;
+	spi.Init.CRCPolynomial = 10;
 	
 	if (HAL_SPI_Init(&spi) != HAL_OK)
 	{
@@ -54,55 +58,56 @@ void GenerateTestSPISignal()
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 	
-    char message[] = "0123456789";
-	char READ_R0[] = { 0x04 };
-	char READ_R1[] = { 0x05 };
-	char READ_R2[] = { 0x06 };
-	char READ_R3[] = { 0x07 };
-	char READ_R4[] = { 0x08 };
-	char READ_R5[] = { 0x09 };
-	char READ_R6[] = { 0x0A };
-	char READ_R7[] = { 0x0B };
+	char response[2];
+	char READ_R0[2] = { 0x00, 0x00 };
+	char READ_R1[2] = { 0x01, 0x00 };
+	char READ_R2[2] = { 0x02, 0x00 };
+	char READ_R3[2] = { 0x03, 0x00 };
+	char READ_R4[2] = { 0x04, 0x00 };
 
 	int w, x, y, z;
 	
+	HAL_StatusTypeDef spi_status;
+	
+	uint8_t command = 13;
+	uint8_t status;
+	uint8_t read_val;
+	uint8_t RX_ADDR_P2 = 12;
+	uint8_t RX_ADDR_P3 = 13;
+	uint8_t RX_ADDR_P4 = 14;
+	uint8_t RX_ADDR_P5 = 15;
+
 	forever
 	{
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R0, sizeof(READ_R0), HAL_MAX_DELAY);
+		spi_status = HAL_SPI_TransmitReceive(&spi, &RX_ADDR_P2, &status, 1, HAL_MAX_DELAY);
+		spi_status = HAL_SPI_Receive(&spi, &read_val, 1, HAL_MAX_DELAY);
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
-		
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R1, sizeof(READ_R0), HAL_MAX_DELAY);
+		spi_status = HAL_SPI_TransmitReceive(&spi, &RX_ADDR_P3, &status, 1, HAL_MAX_DELAY);
+		spi_status = HAL_SPI_Receive(&spi, &read_val, 1, HAL_MAX_DELAY);
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
-		
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R2, sizeof(READ_R0), HAL_MAX_DELAY);
+		spi_status = HAL_SPI_TransmitReceive(&spi, &RX_ADDR_P4, &status, 1, HAL_MAX_DELAY);
+		spi_status = HAL_SPI_Receive(&spi, &read_val, 1, HAL_MAX_DELAY);
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
-		
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R3, sizeof(READ_R0), HAL_MAX_DELAY);
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
-
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R4, sizeof(READ_R0), HAL_MAX_DELAY);
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
-		
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R5, sizeof(READ_R0), HAL_MAX_DELAY);
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
-		
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R6, sizeof(READ_R0), HAL_MAX_DELAY);
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
-		
-		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
-		HAL_SPI_Transmit(&spi, (uint8_t *)READ_R7, sizeof(READ_R0), HAL_MAX_DELAY);
+		spi_status = HAL_SPI_TransmitReceive(&spi, &RX_ADDR_P5, &status, 1, HAL_MAX_DELAY);
+		spi_status = HAL_SPI_Receive(&spi, &read_val, 1, HAL_MAX_DELAY);
 		HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
 
 		HAL_Delay(10);
-
 	}
+}
+
+void spi_cs_lo()
+{
+	HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_RESET);
+}
+
+void spi_cs_hi()
+{
+	HAL_GPIO_WritePin(GPIOA, SPI_CS, GPIO_PIN_SET);
 }
  
 int main(void)
