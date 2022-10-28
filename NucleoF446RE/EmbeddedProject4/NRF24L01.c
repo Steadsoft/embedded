be0f9ec76f26164e5c863a9d9591f9f8d3b968fa
Hugh <hugh_gleaves@hotmail.com>
2022-10-28 10:17:15 -0700

    Make library functions private and reorg the init code.

--- NucleoF446RE/EmbeddedProject4/NRF24L01.c
+++ NucleoF446RE/EmbeddedProject4/NRF24L01.c
@@ -6,17 +6,31 @@
 
 static void spi_cs_lo(NrfSpiDevice *);
 static void spi_cs_hi(NrfSpiDevice *);
+static void _InitializeDevice(SPI_HandleTypeDef * SpiPtr, GPIO_TypeDef * GpioPtr, uint8_t CsPin, uint8_t CePin, NrfSpiDevice * Device);
+static void _ReadSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus);
+static void _WriteSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus);
+static void _ReadMultiBytesRegister(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesRead, STATUS * NrfStatus);
+static void _WriteMultiBytesRegister(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesWritten, STATUS * NrfStatus);
 
 // SEE: https://www.mouser.com/datasheet/2/297/nRF24L01_Product_Specification_v2_0-9199.pdf
 
-void _InitializeDevice(SPI_HandleTypeDef * SpiPtr, GPIO_TypeDef * GpioPtr, uint8_t CsPin, uint8_t CePin, NrfSpiDevice * Device)
+void InitializeLibrary(NrfLibrary * lib)
+{
+	lib->InitializeDevice = _InitializeDevice;
+	lib->ReadSingleByteRegister = _ReadSingleByteRegister;
+	lib->WriteSingleByteRegister = _WriteSingleByteRegister;
+	lib->ReadMultiBytesRegister = _ReadMultiBytesRegister;
+	lib->WriteMultiBytesRegister = _WriteMultiBytesRegister;
+}
+
+static void _InitializeDevice(SPI_HandleTypeDef * SpiPtr, GPIO_TypeDef * GpioPtr, uint8_t CsPin, uint8_t CePin, NrfSpiDevice * Device)
 {
 	Device->spi_ptr = SpiPtr;
 	Device->gpio_ptr = GpioPtr;
 	Device->cs_pin = CsPin;
 	Device->ce_pin = CePin;
 }
-void _ReadSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus)
+static void _ReadSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus)
 {
 	uint8_t command = R_REGISTER | Register;
 	spi_cs_lo(SPI);
@@ -24,7 +38,7 @@ void _ReadSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value,
 	SPI->status = HAL_SPI_Receive(SPI->spi_ptr, Value, 1, HAL_MAX_DELAY);
 	spi_cs_hi(SPI);
 }
-void _WriteSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus)
+static void _WriteSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus)
 {
 	uint8_t command = W_REGISTER | Register;
 	spi_cs_lo(SPI);
@@ -32,7 +46,7 @@ void _WriteSingleByteRegister(NrfSpiDevice * SPI, uint8_t Register, void * Value
 	SPI->status = HAL_SPI_Transmit(SPI->spi_ptr, Value, 1, HAL_MAX_DELAY);
 	spi_cs_hi(SPI);
 }
-void _ReadMultiBytesRegister(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesRead, STATUS * NrfStatus)
+static void _ReadMultiBytesRegister(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesRead, STATUS * NrfStatus)
 {
 	uint8_t command = R_REGISTER | Register;
 	uint8_t width;
@@ -60,7 +74,7 @@ void _ReadMultiBytesRegister(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value
 	SPI->status = HAL_SPI_Receive(SPI->spi_ptr, Value, bytes, HAL_MAX_DELAY);
 	spi_cs_hi(SPI);
 }
-void _WriteMultiBytesRegister(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesWritten, STATUS * NrfStatus)
+static void _WriteMultiBytesRegister(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesWritten, STATUS * NrfStatus)
 {
 	uint8_t command = W_REGISTER | Register;
 	uint8_t width;
--- NucleoF446RE/EmbeddedProject4/NRF24L01_types.h
+++ NucleoF446RE/EmbeddedProject4/NRF24L01_types.h
@@ -124,3 +124,11 @@ typedef struct
 
 } EN_RXADDR;
 
+typedef struct
+{
+	void(* InitializeDevice)(SPI_HandleTypeDef * SpiPtr, GPIO_TypeDef * GpioPtr, uint8_t CsPin, uint8_t CePin, NrfSpiDevice * Device);
+	void(* ReadSingleByteRegister)(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus);
+	void(* WriteSingleByteRegister)(NrfSpiDevice * SPI, uint8_t Register, void * Value, STATUS * NrfStatus);
+	void(* ReadMultiBytesRegister)(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesRead, STATUS * NrfStatus);
+	void(* WriteMultiBytesRegister)(NrfSpiDevice * SPI, uint8_t Register, uint8_t Value[], uint8_t * BytesWritten, STATUS * NrfStatus);
+} NrfLibrary;
