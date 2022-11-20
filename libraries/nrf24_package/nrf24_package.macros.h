61e91f5758646025adf659802574279446d98188
Hugh <hugh_gleaves@hotmail.com>
2022-11-20 16:27:57 -0700

    Tiny comment improvement

--- libraries/nrf24_package/nrf24_package.macros.h
+++ libraries/nrf24_package/nrf24_package.macros.h
@@ -17,6 +17,7 @@
 	// Convert any register struct into a unisgned int
 	#define REG_TO_INT(Register) (*(uint8_t*)(&(Register)))
 
-	// Compute the result register from a Current value after applying New values as indicated by a Mask struct
+	// Compute the result register from a Current value after applying New bit values as indicated by a Mask struct
 	#define TWIDDLE(Current,New,Mask) (~(REG_TO_INT(Mask)) & REG_TO_INT(Current)) | (REG_TO_INT(Mask) & REG_TO_INT(New))
-#endif
+#endif
+
