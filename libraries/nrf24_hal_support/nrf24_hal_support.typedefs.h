322592afe65b49306ef5f1afbe322f5499b010ef
Hugh <hugh_gleaves@hotmail.com>
2022-11-20 12:29:43 -0700

    Copy the two library's files into folders.

---
+++ libraries/nrf24_hal_support/nrf24_hal_support.typedefs.h
@@ -0,0 +1,6 @@
+typedef struct nrf24_io_descriptor NrfIoDescriptor, * NrfIoDescriptor_ptr;
+typedef const struct nrf24_hal_support_interface nrf24_hal_support_struct, * nrf_hal_support_ptr;
+
+#if defined(nrf24_hal_support_implementer)
+// Private typedefs can go here, these will never been seen by consumer code when compiled.
+#endif
---
+++ libraries/nrf24_package/nrf24_package.externs.h
@@ -0,0 +1,4 @@
+/* This file is for consumption only by consumer code. */
+/* Global singletons exposed by the NRF library.       */
+extern nrf24_register_names Nrf24Register;
+extern nrf24_package_struct nrf24_package;
