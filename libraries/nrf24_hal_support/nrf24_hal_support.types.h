99f5e65a29feeb6173145db79f5df451a161bf10
Hugh <hugh_gleaves@hotmail.com>
2025-04-03 11:35:43 -0700

    Use Doxygen style comments that work with intellisense.

--- libraries/nrf24_hal_support/nrf24_hal_support.types.h
+++ libraries/nrf24_hal_support/nrf24_hal_support.types.h
@@ -1,9 +1,32 @@
 struct nrf24_hal_support_interface
 {
+	/**
+	 * @brief Configures the selected SPI instance and associated GPIO pins
+	 * for SPI to function. The GPIO port is implied by the SPI base
+	 * address, the interrupt, CE and CS pins are also configured and must be in the same GPIO port as
+	 * the chosen SPI instance.
+	 * @param spi_base The base address of the SPI instance to be configured.
+	 * @param int_pin The GPIO pin number for the interrupt line (negative value means no interrupt).
+	 * @param ce_pin The GPIO pin number for the CE (Chip Enable) line.
+	 * @param cs_pin The GPIO pin number for the CSN (Chip Select Not) line.
+	 * @param device_ptr The returned configured device instance. 
+	*/
 	void(*Configure)(uint32_t spi_base, int32_t int_pin, uint32_t ce_pin, uint32_t cs_pin, NrfSpiDevice_ptr device_ptr);
+	/** 
+	 Sets the NRF device's CE pin to low
+	 */
 	void(*Deactivate)(NrfSpiDevice_ptr);
+	/** 
+	 Sets the NRF device's CE pin to high 
+	 */
 	void(*Activate)(NrfSpiDevice_ptr);
+	/**
+	 Sets the NRF device's SPI CSN pin to low 
+	 */
 	void(*Select)(NrfSpiDevice_ptr);
+	/// <summary>
+	/// Sets the NRF device's SPI CSN pin to high 
+	/// </summmary>
 	void(*Deselect)(NrfSpiDevice_ptr);
 	void(*ExchangeBytes)(NrfSpiDevice_ptr, uint8_t[], uint8_t[], uint8_t);
 	void(*ReadBytes)(NrfSpiDevice_ptr, uint8_t bytes_in_ptr[], uint8_t count);
